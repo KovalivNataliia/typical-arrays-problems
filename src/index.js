f7119b2d317c2ed936155825c8e97cf4b27985c2
NataliiaKovaliv <nataliiakovaliv95@gmail.com>
2021-07-20 22:27:22 +0300

    refactor: delete console.log

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 
 exports.min = function min (array) {
-  console.log(array)
   return array === undefined || array.length === 0 ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  console.log(array)
   return array === undefined || array.length === 0 ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  console.log(array)
   return array === undefined || array.length === 0 ? 0 : array.reduce((a, b) => a + b) / array.length;
 }
